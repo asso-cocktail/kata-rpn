109edcd59046fd1b051f540bbf90e0f6afa43ed3
Emmanuel Turbet <manustrozor@hesothread.com>
2018-07-27 00:56:41 +0200

    exercise RPN Calculator

---
+++ src/main/java/kata/rpn/RPNCalculator.java
@@ -0,0 +1,143 @@
+package kata.rpn;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
+
+/**
+ * Calculate RPN expressions
+ * @author Emmanuel Turbet-Delof
+ * @version 1.0
+ */
+public class RPNCalculator implements Calculator {
+
+    private int roundingScale;
+
+    private IllegalArgumentException invalidArgumentException = new IllegalArgumentException("Invalid expression");
+    private ArithmeticException divideByZeroException = new ArithmeticException("/ by zero");
+
+    RPNCalculator(int roundingScale)
+    {
+        this.roundingScale = roundingScale;
+    }
+
+    RPNCalculator()
+    {
+        this.roundingScale = 1;
+    }
+
+    /**
+     * Calculate a RPN expression
+     * @param operation (String) RPN expression
+     * @return (BigDecimal) the result
+     */
+    public BigDecimal compute(String operation)
+    {
+        if(!isValid(operation))
+            throw this.invalidArgumentException;
+
+        String[] stack = new String[0];
+
+        for (String elem : operation.split(" "))
+            stack = isNum(elem) ? push(stack, elem) : calc(stack, elem);
+
+        if (stack.length > 1)
+            throw this.invalidArgumentException;
+
+        return new BigDecimal(stack[0]);
+    }
+
+    /**
+     * Determines if the RPN expression is valid
+     * @param operation RPN expression
+     * @return (boolean) true or false
+     */
+    private boolean isValid(String operation)
+    {
+        String[] n = operation.split(" ");
+
+        if (n.length == 0) return false;
+        for (String i : n)
+            if (!isSign(i) && !isNum(i)) return false;
+        return true;
+    }
+
+    /**
+     * Determines if an element is a valid mathematical sign
+     * @param elem (String) element
+     * @return (boolean) true or false
+     */
+    private boolean isSign(String elem)
+    {
+        return elem.equals("+") || elem.equals("-") || elem.equals("x") || elem.equals("/");
+    }
+
+    /**
+     * Determines if an element is a number
+     * @param elem (String) element
+     * @return (boolean) true or false
+     */
+    private boolean isNum(String elem)
+    {
+        try {
+            Double.parseDouble(elem);
+        }catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Adds elem to the end of array
+     * @param array (String[]) array that contains elements
+     * @param elem (String) element of the array
+     * @return (String[]) a new array grown by one element
+     */
+    private String[] push(String[] array, String elem)
+    {
+        String[] copy = Arrays.copyOf(array, array.length+1);
+        copy[array.length] = elem;
+        return copy;
+    }
+
+    /**
+     * Make an addition, subtraction, multiplication or division
+     * on the last two elements of the array depends on the mathematical sign given
+     * @param array (String[]) array that contains elements to be calculated
+     * @param sign (String) mathematical sign : +, -, x or /
+     * @return (String[]) a new array with a result at the end instead of two elements
+     * or exit the program if the calculation is a division by 0
+     * or exit the program if the sign is invalid (not equal to +, -, x or /)
+     */
+    private String[] calc(String[] array, String sign)
+    {
+        int pos = array.length - 2;
+
+        BigDecimal first = new BigDecimal(array[pos]);
+        BigDecimal second = new BigDecimal(array[pos+1]);
+
+        switch(sign) {
+            case "+":
+                array[pos] = first.add(second).toString();
+                break;
+            case "-":
+                array[pos] = first.subtract(second).toString();
+                break;
+            case "x":
+                array[pos] = first.multiply(second).toString();
+                break;
+            case "/":
+                if (second.equals(BigDecimal.ZERO))
+                    throw this.divideByZeroException;
+                BigDecimal res = first.divide(second, this.roundingScale, RoundingMode.FLOOR);
+                if(Double.parseDouble(res.toString()) - Math.floor(Double.parseDouble(res.toString())) == 0)
+                    res = res.setScale(0,BigDecimal.ROUND_DOWN);
+                array[pos] = res.toString();
+                break;
+            default:
+                break;
+        }
+
+        return Arrays.copyOf(array, array.length-1);
+    }
+}
--- src/test/java/kata/rpn/CalculatorTest.java
+++ src/test/java/kata/rpn/CalculatorTest.java
@@ -1,38 +1,71 @@
 package kata.rpn;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculatorTest {
 
+    private RPNCalculator rpn;
+
+    public CalculatorTest()
+    {
+        rpn = new RPNCalculator();
+    }
+
     @Test
     public void additionSimple() {
-        assertFalse(true);
+        assertTrue(rpn.compute("4 2 +").equals(new BigDecimal(6)));
+        assertTrue(rpn.compute("0 0 +").equals(new BigDecimal(0)));
+        assertTrue(rpn.compute("2 2 +").equals(new BigDecimal(4)));
+        assertTrue(rpn.compute("-4 4 +").equals(new BigDecimal(0)));
+        assertTrue(rpn.compute("2 -2 +").equals(new BigDecimal(0)));
+        assertTrue(rpn.compute("-4 -2 +").equals(new BigDecimal(-6)));
     }
 
     @Test
     public void soustractionSimple() {
-        assertFalse(true);
+        assertTrue(rpn.compute("4 2 -").equals(new BigDecimal(2)));
+        assertTrue(rpn.compute("0 0 -").equals(new BigDecimal(0)));
+        assertTrue(rpn.compute("2 2 -").equals(new BigDecimal(0)));
+        assertTrue(rpn.compute("-4 4 -").equals(new BigDecimal(-8)));
+        assertTrue(rpn.compute("2 -2 -").equals(new BigDecimal(4)));
+        assertTrue(rpn.compute("-4 -2 -").equals(new BigDecimal(-2)));
     }
 
     @Test
     public void multiplicationSimple() {
-        assertFalse(true);
+        assertTrue(rpn.compute("1 0 x").equals(new BigDecimal(0)));
+        assertTrue(rpn.compute("1 1 x").equals(new BigDecimal(1)));
+        assertTrue(rpn.compute("3 6 x").equals(new BigDecimal(18)));
+        assertTrue(rpn.compute("5 25 x").equals(new BigDecimal(125)));
+        assertTrue(rpn.compute("11 123 x").equals(new BigDecimal(1353)));
     }
 
     @Test
     public void divisionSimple() {
-        assertFalse(true);
+        assertTrue(rpn.compute("1 1 /").equals(new BigDecimal(1)));
+        assertTrue(rpn.compute("1 2 /").equals(new BigDecimal(0.5)));
+        // BigDecimal(0.3) = 0.29999999...
+        assertTrue(rpn.compute("1 3 /").equals(new BigDecimal("0.3")));
+        assertTrue(rpn.compute("3 2 /").equals(new BigDecimal(1.5)));
     }
 
     @Test
     public void operationComposee() {
-        assertFalse(true);
+        assertTrue(rpn.compute("3 15 8 x 7 + x").equals(new BigDecimal(381)));
+        assertTrue(rpn.compute("1 2 + 4 x 5 + 3 -").equals(new BigDecimal(14)));
+        assertTrue(rpn.compute("5 4 1 2 + x +").equals(new BigDecimal(17)));
+        assertTrue(rpn.compute("3 4 2 1 + x + 2 /").equals(new BigDecimal(7.5)));
     }
 
     @Test
     public void divisionParZero() {
-        assertFalse(true);
+        assertThrows(ArithmeticException.class, () -> rpn.compute("42 0 /"));
+        assertThrows(ArithmeticException.class, () -> rpn.compute("3 4 2 1 + x + 0 /"));
     }
 
 }
